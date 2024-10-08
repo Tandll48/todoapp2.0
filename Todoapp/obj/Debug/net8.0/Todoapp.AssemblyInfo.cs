e77456d0c6839b328f5e1f99af501cc886244883
Leo Tandl <leojotandl@gmail.com>
2024-10-08 23:05:27 +0200

    console with different user

--- Todoapp/obj/Debug/net8.0/Todoapp.AssemblyInfo.cs
+++ Todoapp/obj/Debug/net8.0/Todoapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todoapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc2c85dc153b7a84b37a51d105e7e73dcbf1157")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a8f454ffb1f67d6bd018ee3bd62fad4936e4380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todoapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todoapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Todoapp/todoapplication.cs
+++ Todoapp/todoapplication.cs
@@ -30,52 +30,128 @@ public class ConsolApplication{
         }
 
 
-        Request("CREATE DATABASE IF NOT EXISTS todoapplication; USE todoapplication; CREATE TABLE IF NOT EXISTS Liste(id INT AUTO_INCREMENT PRIMARY KEY,Name TEXT); CREATE TABLE IF NOT EXISTS aufgabe ( id INT AUTO_INCREMENT PRIMARY KEY, beschreibung TEXT, priorität INT CHECK (priorität BETWEEN 0 AND 5), listen_id INT REFERENCES liste(id) );");
-        
-        Console.WriteLine("PROLQS-Datenbankanwendung");
+        string RequestVariable(string queryString){
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                MySqlCommand command = new MySqlCommand(queryString, connection);
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
 
-                while (true)
-                {
-                    Console.WriteLine("\nWählen Sie eine Option:");
-                    Console.WriteLine("1. Anlegen einer neuen Liste");
-                    Console.WriteLine("2. Anlegen einer neuen Aufgabe");
-                    Console.WriteLine("3. Ausgabe der Aufgaben sortiert nach Priorität");
-                    Console.WriteLine("4. Ausgabe aller Aufgaben einer Liste");
-                    Console.WriteLine("5. Aufgabe als erledigt markieren");
-                    Console.WriteLine("6. Ausgabe aller Aufgaben die erledigt sind");
-                    Console.WriteLine("7. Aufgabe die erledigt ist wieder zurückholen");
-                    Console.WriteLine("8. Beenden");
-    
-                    Console.Write("Auswahl: ");
-                    string ?input = Console.ReadLine();
-        
-                    switch (input)
-                    {
-                        case "1":
-                            AddNewList();
-                            break;
-                        case "2":
-                            AddNewTask();
-                            break;
-                        case "3":
-                            TaskOutput();
-                            break;
-                        case "4":
-                            TasksfromList();
-                            break;
-                        case "5":
-                            CheckTask();
-                            break;
-                        case "6":
-                            FinishedTasks();
-                            break;
-                        case "7":
-                            RestoreTask();
-                            break;
-                        case "8":
-                            return;
-                    }
+                string output = "";
+                    while (reader.Read())
+                        {
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                output += reader[i].ToString();
+                            }
+                        }
+                reader.Close();
+                return output;
+            }
+        }
+
+
+        Request("CREATE DATABASE IF NOT EXISTS todoapplication; USE todoapplication; CREATE TABLE IF NOT EXISTS Liste(id INT AUTO_INCREMENT PRIMARY KEY,Name TEXT); CREATE TABLE IF NOT EXISTS aufgabe ( id INT AUTO_INCREMENT PRIMARY KEY, beschreibung TEXT, priorität INT CHECK (priorität BETWEEN 0 AND 5), listen_id INT REFERENCES liste(id) );");
+        Console.WriteLine("PROLQS-Datenbankanwendung");
+        string activeUser = "";
+        LoginToDo();
+        void LoginToDo(){
+            Console.WriteLine("AnmeldeFenster");
+            while (true){
+                Console.WriteLine("1. Anmelden");
+                Console.WriteLine("2. Registrieren");
+                Console.WriteLine("3. Beenden");
+                Console.Write("Auswahl: ");
+                string ?input = Console.ReadLine();
+                switch (input){
+                    case "1":
+                        Login();
+                        break;
+                    case "2":
+                        SignIn();
+                        break;
+                    case "3":
+                        return;
                 }
+            }
+        }
+
+        void Login(){
+            Console.WriteLine("Email-Adresse:");
+            string ?email = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string ?password = Console.ReadLine();
+            string realpassword = RequestVariable("SELECT password FROM `user` WHERE email ='"+email+"';");
+            if (realpassword == password){
+                Console.WriteLine("Login Successful");
+                activeUser = email;
+                MainConsole();
+            }
+            else{
+                Console.WriteLine("Wrong Username or Password");
+                return;}
+            
+        }
+
+        void SignIn(){
+            Console.WriteLine("Email-Adresse:");
+            string ?email = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string ?password = Console.ReadLine();
+            Request("INSERT INTO `user`(`email`, `password`) VALUES ('"+email+"','"+password+"');");
+            MainConsole();
+        }
+        
+        
+        
+        
+        
+        
+        void MainConsole(){
+            Console.WriteLine("Active user: "+activeUser);
+                    while (true)
+                    {
+                        Console.WriteLine("\nWählen Sie eine Option:");
+                        Console.WriteLine("1. Anlegen einer neuen Liste");
+                        Console.WriteLine("2. Anlegen einer neuen Aufgabe");
+                        Console.WriteLine("3. Ausgabe der Aufgaben sortiert nach Priorität");
+                        Console.WriteLine("4. Ausgabe aller Aufgaben einer Liste");
+                        Console.WriteLine("5. Aufgabe als erledigt markieren");
+                        Console.WriteLine("6. Ausgabe aller Aufgaben die erledigt sind");
+                        Console.WriteLine("7. Aufgabe die erledigt ist wieder zurückholen");
+                        Console.WriteLine("8. Ausloggen");
+        
+                        Console.Write("Auswahl: ");
+                        string ?input = Console.ReadLine();
+            
+                        switch (input)
+                        {
+                            case "1":
+                                AddNewList();
+                                break;
+                            case "2":
+                                AddNewTask();
+                                break;
+                            case "3":
+                                TaskOutput();
+                                break;
+                            case "4":
+                                TasksfromList();
+                                break;
+                            case "5":
+                                CheckTask();
+                                break;
+                            case "6":
+                                FinishedTasks();
+                                break;
+                            case "7":
+                                RestoreTask();
+                                break;
+                            case "8":
+                                return;
+                        }
+                    }
+                }   
 
                 void AddNewList(){
                     Console.WriteLine("Name der neuen Liste: ");
@@ -90,7 +166,7 @@ public class ConsolApplication{
                     string ?name = Console.ReadLine();
                     Console.WriteLine("Priorität der Aufgabe (0(niedrig)- 5(hoch)):");
                     string ?priorität = Console.ReadLine();   
-                    Request("INSERT INTO aufgabe (beschreibung,priorität,listen_id) VALUES ('"+name+"',"+priorität+","+list+");");
+                    Request("INSERT INTO aufgabe (beschreibung,priorität,listen_id, user_email) VALUES ('"+name+"',"+priorität+","+list+",'"+activeUser+"');");
                     
                 }
                 void TaskOutput(){
@@ -99,10 +175,10 @@ public class ConsolApplication{
                     Console.WriteLine("\nListe    Beschreibung    Priorität");
                     switch (input){
                         case "1":
-                            Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0 ORDER BY priorität DESC;");
+                            Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0 AND aufgabe.user_email = '"+activeUser+"' ORDER BY priorität DESC;");
                             break;
                         case "2":
-                            Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0 ORDER BY priorität ASC;");
+                            Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0 AND aufgabe.user_email = '"+activeUser+"' ORDER BY priorität ASC;");
                             break;
                     }
                 }
@@ -111,12 +187,12 @@ public class ConsolApplication{
                     Console.WriteLine("\nID der Liste die ausgegeben wird: ");
                     string ?list = Console.ReadLine();
                     Console.WriteLine("\nListe    Beschreibung    Priorität");
-                    Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE listen_ID = "+list+" AND isCompleted = 0;");
+                    Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE listen_ID = "+list+" AND isCompleted = 0 AND aufgabe.user_email = '"+activeUser+"';");
                 }
 
                 void CheckTask(){
                     Console.WriteLine("\nID     Liste    Beschreibung    Priorität");
-                    Request("SELECT aufgabe.id, liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0");
+                    Request("SELECT aufgabe.id, liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 0 AND aufgabe.user_email = '"+activeUser+"'");
                     Console.WriteLine("Welche Aufgabe soll abgehakt werden?(ID)");
                     string ?task = Console.ReadLine();
                     Request("UPDATE aufgabe SET isCompleted = 1 WHERE id = "+task+";");
@@ -125,12 +201,12 @@ public class ConsolApplication{
 
                 void FinishedTasks(){
                     Console.WriteLine("\nListe    Beschreibung    Priorität");
-                    Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 1");
+                    Request("SELECT liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 1 AND aufgabe.user_email = '"+activeUser+"'");
                 }
 
                 void RestoreTask(){
                     Console.WriteLine("\nID     Liste    Beschreibung    Priorität");
-                    Request("SELECT aufgabe.id, liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 1");
+                    Request("SELECT aufgabe.id, liste.name, aufgabe.beschreibung, aufgabe.priorität FROM `aufgabe` JOIN  `liste` ON liste.id = aufgabe.listen_id WHERE isCompleted = 1 AND aufgabe.user_email = '"+activeUser+"'");
                     Console.WriteLine("ID der wiederherzustellenden Aufgabe: ");
                     string ?task = Console.ReadLine();
                     Request("UPDATE aufgabe SET isCompleted = 0 WHERE id = "+task+";");
